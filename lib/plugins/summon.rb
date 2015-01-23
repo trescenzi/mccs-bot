d86b8c44e67d14335f1f9fac76e2f94f550534df
trescenzi <trescenzi@gmail.com>
2015-01-23 19:38:04 +0000

    turning on safe search for initial summons... you have been warned

--- lib/plugins/summon.rb
+++ lib/plugins/summon.rb
@@ -6,13 +6,13 @@ class Summon
 
   match(/summon (.+)/, method: :single_summon)
   def single_summon(m, query)
-    image_results = Google::Search::Image.new(:query => query)
+    image_results = Google::Search::Image.new(:query => query, :image_size => :medium, :saftey_level => :active)
     m.reply(image_results.first.uri.to_s)
   end
 
   match(/resummon (.+)/, method: :resummon)
   def resummon(m, query)
-    image_results = Google::Search::Image.new(:query => query)
+    image_results = Google::Search::Image.new(:query => query, :size => :medium)
     image_results = image_results.to_a
     image_results.shift
     m.reply(image_results.sample.uri.to_s)
