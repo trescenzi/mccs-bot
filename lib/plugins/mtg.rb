6964581ec66c52044c4023fd27750f0405c049ac
trescenzi <trescenzi@gmail.com>
2015-01-23 12:20:34 +0000

    Added support for aether as Æther

--- lib/plugins/mtg.rb
+++ lib/plugins/mtg.rb
@@ -7,6 +7,16 @@ class MTG
   @@file = File.read("cards.json");
   @@cards = JSON.parse(@@file);
 
+  def aetherize(s)
+    words = s.split
+    words.each do |word|
+      if (word.casecmp("aether") == 0)
+        word.sub!(/[Aa][Ee]/,"\u00C6")
+      end
+    end
+    words.join(" ").squeeze(" ")
+  end
+
   def titleCase(s)
     title = s.split
     title.each_with_index.map do |word, i|
@@ -19,7 +29,8 @@ class MTG
 
   match(/card (.+)/)
   def execute(m, target)
-    card = @@cards[titleCase(target)]
+    puts aetherize(titleCase(target))
+    card = @@cards[aetherize(titleCase(target))]
     link = URI::encode("http://mtgimage.com/card/"+card["imageName"]+".jpg")
     m.reply link
   end
